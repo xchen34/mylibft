e68f908525ddb0ecb5cd1f18e5ac77bfbfc2a623
zzy <118268535+xchen34@users.noreply.github.com>
2023-11-10 14:06:32 +0100

    Add files via upload

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,12 +6,12 @@
 /*   By: leochen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 17:23:43 by leochen           #+#    #+#             */
-/*   Updated: 2023/11/09 18:13:21 by leochen          ###   ########.fr       */
+/*   Updated: 2023/11/10 13:10:28 by leochen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
+//#include <stdio.h>
+//#include <string.h>
 #include "libft.h"
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
@@ -19,40 +19,32 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	i;
 	size_t	j;
 	char	*b;
-	
+
 	b = (char *)big;
 	i = 0;
-	j = 0;
 	if (*little == '\0')
 		return (b);
 	while (*b && i < len)
 	{
-		while (*little && )
-		{
-			if (b[i] != little[j])
-				i ++;
-			else
-			{
-				j ++;
-				i = 0;
-			}
-		}
-		return (&(b[i]) - ft_strlen(little));	
+		j = 0;
+		while (*little && i + j < len && big[i + j] == little[j]
+			&& b[i + j] != '\0')
+			j ++;
+		if (little[j] == '\0')
+			return (&b[i]);
+		i ++;
 	}
 	return (NULL);
 }
 
-
-int	main()
+/*int	main()
 {
-	char lit[] = "const";
-	char big[] = "iloconstance";
+	char lit[] = "constdfdfsfff";
+	char big[] = "constance";
 	
-	printf("%p\n", ft_strnstr(big, lit, 5));
+	printf("%p\n", ft_strnstr(big, lit, 0));
 //	printf("%p\n", strnstr(big, lit, 5));
 
 	return 0;
 
-}
-
-
+}*/
