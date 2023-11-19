12461d7e6d7b9764deb37f4f6181f41be1d36a95
zzy <118268535+xchen34@users.noreply.github.com>
2023-11-20 00:15:06 +0100

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -5,160 +5,84 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: leochen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/14 12:49:20 by leochen           #+#    #+#             */
-/*   Updated: 2023/11/16 19:11:05 by leochen          ###   ########.fr       */
+/*   Created: 2023/11/19 20:42:10 by leochen           #+#    #+#             */
+/*   Updated: 2023/11/20 00:11:13 by czha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<stdio.h>
-#include<stdlib.h>
-#include "libft.h"
 
-static int	count_words(char const *s, char c)
+#include"libft.h"
+
+static char	**malloc_fail(char **split, int word)
 {
-	int	i;
-	int	count;
-
-	i = 0;
-	count = 0;
-	while (s[i])
-	{
-		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
-			count++;
-		i++;
-	}
-	return (count);
+	while (word > 0)
+		free(split[word--]);
+	free(split);
+	return (NULL);
 }
 
-static int	split_words(char **result, char const *s, char c, int word)
-{
-	int		start_cur;
-	int		end_cur;
-
-	end_cur = 0;
-	start_cur = 0;
-	while (s[end_cur])
-	{
-		if (s[end_cur] == c || s[end_cur] == 0)
-			start_cur = end_cur + 1;
-		if (s[end_cur] != c && (s[end_cur + 1] == c || s[end_cur + 1] == 0))
-		{
-			result[word] = malloc(sizeof(char) * (end_cur - start_cur + 2));
-			if (!result[word])
-			{
-				while (word++)
-					free(result[word]);
-				return (0);
-			}
-			ft_strlcpy(result[word], (s + start_cur), end_cur - start_cur + 2);
-			word++;
-		}
-		end_cur++;
-	}
-	result[word] = 0;
-	return (1);
-}
-
-char	**ft_split(char const *s, char c)
-{
-	char	**result;
-
-	if (!s)
-		return (NULL);
-	result = malloc(sizeof(char *) * (count_words(s, c) + 1));
-	if (!result)
-		return (NULL);
-	if (!split_words(result, s, c, 0))
-		return (NULL);
-	return (result);
-}
-/*static int	count_word(char const *s, char c)
+static int	count_words(char *s, char c)
 {
 	int	count;
-	int	trigger;
+	int	flag;
 
 	count = 0;
-	trigger = 0;
+	flag = 0;
 	while (*s)
 	{
-		if (*s == c)
-			trigger = 0;
-		else if (*s != c && trigger == 0)
+		if (*s == c && flag == 1)
+			flag = 0;
+		else if (*s != c && flag == 0)
+		{
 			count++;
+			flag = 1;
+		}	
 		s++;
 	}
 	return (count);
 }
 
-static char	*write_word(char const *s, size_t start, size_t end)
+
+char	*write_word(char *s, size_t start, size_t end)
 {
 	char	*word;
-	int		i;
 
-	i = 0;
 	word = (char *)malloc(sizeof(char) * (end - start + 2));
 	if (word == NULL)
 		return (NULL);
-	while (*s && start <= end)
-	{
-		word[i] = s[start];
-		start++;
-		i++;
-	}	
-	word[i] = '\0';
+	if (start <= end)
+		ft_memcpy(word, s + start, end - start + 1);
+	word[end - start + 1] = '\0';
 	return (word);
 }
 
-char	**ft_split(char const *s, char c)
+char **ft_split(char *s, char c)
 {
-	char	**split;
 	size_t	i;
 	size_t	j;
-	int		index;
+	int	index;
+	char	**split;
 
-	split = (char **)malloc(sizeof(char *) * (count_word(s, c) + 1));
-	if (split == NULL || s == NULL)
+	if (!(split = (char **)malloc(sizeof(char *) * (count_words(s, c) + 1))) || !s )
 		return (NULL);
 	i = 0;
 	j = 0;
 	index = -1;
-	while (i < ft_strlen(s))
+	while (s[i])
 	{
 		if (s[i] != c && index < 0)
 			index = i;
-		else if ((s[i] == c || i == ft_strlen(s) - 1) && index >= 0)
+		else if (s[i] == c && index >= 0)
 		{
-			split[j++] = write_word(s, index, (i - 1));
+			split[j] = write_word(s, index, i - 1);
+			if (!split[j])
+				return (malloc_fail(split, j + 1);
 			index = -1;
+			j ++;
 		}
 		i++;
 	}
 	split[j] = 0;
 	return (split);
-}*/
-
-/*int main(void)
-{
-    char const *s = ",,,This,is,a, ,test,,,string,,,,";
-    char c = ',';
-
-    char **result = ft_split(s, c);
-
-    if (result)
-    {
-        // Print each string in the array
-        for (int i = 0; result[i] != NULL; i++)
-        {
-            printf("String %d: %s\n", i + 1, result[i]);
-            free(result[i]); // Don't forget to free each string
-        }
-
-        free(result); // Don't forget to free the array itself
-    }
-    else
-    {
-        printf("Memory allocation failed.\n");
-    }
-
-    return 0;
-}*/
+}
+	
